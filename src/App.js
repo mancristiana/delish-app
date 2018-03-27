a342eb7ab90f7050dbd848d821afd756ddbb033d
Cristiana Man <man.cristiana1@gmail.com>
2018-03-27 12:22:56 +0200

    Added mdbreact dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'mdbreact';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Button color="primary">Primary</Button>
         </p>
       </div>
     );
